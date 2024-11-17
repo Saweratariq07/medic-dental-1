3069fb56058a533115b0d9facbadaae1f07a4c1f
saweratariq07 <saweratariq567@gmail.com>
2024-11-18 01:44:18 +0500

    dent  message-

--- src/components/cont/Cont.jsx
+++ src/components/cont/Cont.jsx
@@ -8,8 +8,11 @@ function Cont() {
 
 
   return (
+    <div>
+       <Heeder/>
+  
     <div className="bg-black min-h-screen flex items-center justify-center pt-24 py-10">
-     <Heeder/>
+    
       <div className="relative w-full max-w-2xl bg-gradient-to-r  from-[#e7cd22] via-[#b29f1f] to-[#373317] p-12 rounded-lg border-black">
         <form  className="space-y-6">
           <div>
@@ -92,6 +95,7 @@ function Cont() {
           </button>
         </form>
       </div>
+    </div>
     </div>
   );
 };
